0b536696b44705e7ca7053e9de9baceab8570bca
Xirantics <88784429+Xirantics@users.noreply.github.com>
2021-08-19 14:21:59 +0200

    best scores between sessions
    The game saves the best three scores and shows them also when a new session starts

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Quit : MonoBehaviour
+{
+    public void Exit()
+    {
+        ScoreManager.SaveScores();
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -21,10 +22,65 @@ public class ScoreManager : MonoBehaviour
         scoreManager = this;
         DontDestroyOnLoad(gameObject);
 
-        for (int i = 0; i < bestScore.Length; i++)
+        LoadScores();
+    }
+
+    // The SaveData class needs to be tagged as serialized to be converted in json format
+    [System.Serializable]
+    class SaveData
+    {
+        public int bestScoreOne;
+        public int bestScoreTwo;
+        public int bestScoreThree;
+
+        public string bestPlayerNameOne;
+        public string bestPlayerNameTwo;
+        public string bestPlayerNameThree;
+    }
+
+    public static void SaveScores()
+    {
+        SaveData data = new SaveData();
+
+        data.bestScoreOne = bestScore[0];
+        data.bestScoreTwo = bestScore[1];
+        data.bestScoreThree = bestScore[2];
+
+        data.bestPlayerNameOne = bestPlayerName[0];
+        data.bestPlayerNameTwo = bestPlayerName[1];
+        data.bestPlayerNameThree = bestPlayerName[2];
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadScores()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
         {
-            bestScore[i] = 0;
-            bestPlayerName[i] = "-";
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            bestScore[0] = data.bestScoreOne;
+            bestScore[1] = data.bestScoreTwo;
+            bestScore[2] = data.bestScoreThree;
+
+            bestPlayerName[0] = data.bestPlayerNameOne;
+            bestPlayerName[1] = data.bestPlayerNameTwo;
+            bestPlayerName[2] = data.bestPlayerNameThree;
+        }
+
+        else
+        {
+            for (int i = 0; i < bestScore.Length; i++)
+            {
+                bestScore[i] = 0;
+                bestPlayerName[i] = "-";
+            }
         }
     }
 
